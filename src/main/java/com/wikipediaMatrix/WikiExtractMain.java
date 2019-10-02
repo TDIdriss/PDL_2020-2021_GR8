53c3d1a8e41c58bca517e06d9723d577b78dd373
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2019-10-02 12:31:58 +0200

    Log4J 2 added

--- src/main/java/com/wikipediaMatrix/WikiExtractMain.java
+++ src/main/java/com/wikipediaMatrix/WikiExtractMain.java
@@ -1,5 +1,8 @@
 package com.wikipediaMatrix;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -24,6 +27,7 @@ public class WikiExtractMain {
 	private static int nbColonnesWikitext;
 	private static int nbLignesWikitext;
 	private static long tempsExeWikitext;
+//	private static final Logger LOG = LogManager.getLogger(WikiExtractMain.class);
 
 	public static void main(String[] args) throws MalformedURLException, IOException, UrlInvalideException, ExtractionInvalideException, ConversionInvalideException, ArticleInexistantException, ResultatEstNullException, InterruptedException {
 
@@ -146,4 +150,13 @@ public class WikiExtractMain {
 		System.out.println("- WIKITEXT - Temps d'execution : " + tempsExeWikitext/1000 + " secondes.");
 		System.out.println("Nombre de tableaux parsés: " + nbTablesWikitext + ", lignes parsées : " + nbLignesWikitext + ", colonnes parsées : " + nbColonnesWikitext);
 	}
+
+//	Logger example
+//	=====================================================
+//	 	LOG.debug("This Will Be Printed On Debug");
+//        LOG.info("This Will Be Printed On Info");
+//        LOG.warn("This Will Be Printed On Warn");
+//        LOG.error("This Will Be Printed On Error");
+//        LOG.fatal("This Will Be Printed On Fatal");
+//	=====================================================
 }

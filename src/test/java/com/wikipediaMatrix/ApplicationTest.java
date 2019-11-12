caf420571b5793972dab8c48bac347ce70e165c6
Koitrin KOFFI <koitrinstephanedesire@gmail.com>
2019-11-12 11:21:03 +0100

    test on csv number and csv format added
    Signed-off-by: Koitrin KOFFI <koitrinstephanedesire@gmail.com>

--- src/test/java/com/wikipediaMatrix/ApplicationTest.java
+++ src/test/java/com/wikipediaMatrix/ApplicationTest.java
@@ -4,8 +4,13 @@ import com.wikipediaMatrix.exception.ExtractionInvalideException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -24,6 +29,7 @@ public class ApplicationTest {
 
 
     @Test
+    @DisplayName("Test pageComporteTableau de l'extracteur Html")
     public void htmlPageComporteTableauTest() throws ExtractionInvalideException, MalformedURLException {
         setup();
         Donnee_Html donneeHtml = new Donnee_Html();
@@ -32,6 +38,7 @@ public class ApplicationTest {
     }
 
     @Test
+    @DisplayName("Test pageComporteTableau de l'extracteur Wikitext")
     public void wikiTextPageComporteTableauTest() throws ExtractionInvalideException, MalformedURLException {
         setup();
         Donnee_Wikitable donneeWikitable = new Donnee_Wikitable();
@@ -40,7 +47,8 @@ public class ApplicationTest {
     }
 
     @Test
-    public void testHtmleExtraction() throws InterruptedException, MalformedURLException {
+    @DisplayName("Test le nombre de tableaux extraits par l'extracteur Html")
+    public void testHtmlExtraction() throws InterruptedException, MalformedURLException {
         setup();
         Donnee_Html donneeHtml = new Donnee_Html();
         donneeHtml.setUrl(ownUrl);
@@ -51,6 +59,7 @@ public class ApplicationTest {
 
 
     @Test
+    @DisplayName("Test le nombre de tableaux extraits par l'extracteur Wikitext")
     public void testWikiTableExtraction() throws InterruptedException, MalformedURLException {
         setup();
         Donnee_Wikitable donneeWikitable = new Donnee_Wikitable();
@@ -60,4 +69,55 @@ public class ApplicationTest {
         assertEquals(3, donneeWikitable.getNbTableaux());
     }
 
+    @Test
+    @DisplayName("Test si l'extracteur Html et Wikitext extraient le même nombre de tableau pour une Url")
+    public void testGetNbTableaux() throws IOException, InterruptedException {
+        String BASE_WIKIPEDIA_URL = "output/large_url_test.txt";
+        BufferedReader br = new BufferedReader(new FileReader(BASE_WIKIPEDIA_URL));
+        String url;
+        while ((url = br.readLine()) != null) {
+            Url wikiUrl = new Url(new URL(url));
+            Donnee_Html donneeHtml = new Donnee_Html();
+            Donnee_Wikitable donneeWikitable = new Donnee_Wikitable();
+            donneeHtml.setUrl(wikiUrl);
+            donneeHtml.start();
+            donneeHtml.join();
+            donneeWikitable.setUrl(wikiUrl);
+            donneeWikitable.start();
+            donneeWikitable.join();
+            assert donneeHtml.getNbTableaux() == donneeWikitable.getNbTableaux();
+        }
+    }
+
+    @Test
+    @DisplayName("Test de la validité du format du csv généré par l'extracteur Wikitext")
+    public void wikitextExtractorTest() throws MalformedURLException, InterruptedException {
+        Url urlTest = new Url(new URL("https://en.wikipedia.org/wiki/Comparison_between_U.S._states_and_countries_by_GDP_(PPP)"));
+        assertTrue(urlTest.estTitreValide());
+        Donnee_Wikitable donneeWikitable = new Donnee_Wikitable();
+        donneeWikitable.setUrl(urlTest);
+        donneeWikitable.start();
+        donneeWikitable.join();
+
+        CSVValidator csvValidator = CSVValidator.getInstance();
+        csvValidator.setPath("wikitext/");
+
+        assertTrue(csvValidator.checkCSV(urlTest.getTitre()+"1.csv"));
+    }
+
+    @Test
+    @DisplayName("Test de la validité du format du csv généré par l'extracteur Html")
+    public void htmlExtractorTest() throws MalformedURLException, InterruptedException {
+        Url urlTest = new Url(new URL("https://en.wikipedia.org/wiki/Comparison_between_U.S._states_and_countries_by_GDP_(PPP)"));
+        assertTrue(urlTest.estTitreValide());
+        Donnee_Html donneeHtml = new Donnee_Html();
+        donneeHtml.setUrl(urlTest);
+        donneeHtml.start();
+        donneeHtml.join();
+
+        CSVValidator csvValidator = CSVValidator.getInstance();
+        csvValidator.setPath("HTML/");
+
+        assertTrue(csvValidator.checkCSV(urlTest.getTitre()+"-01.csv"));
+    }
 }

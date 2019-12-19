bb863d50ca414ebfda0472532d4d27a3406d4d0c
William ZOUNON <williamzounon@gmail.com>
2019-12-19 17:11:11 +0100

    A

--- src/test/java/com/wikipediaMatrix/ComplexTableTest.java
+++ src/test/java/com/wikipediaMatrix/ComplexTableTest.java
@@ -1,5 +1,6 @@
 package com.wikipediaMatrix;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -10,10 +11,28 @@ import static org.junit.Assert.assertTrue;
 
 public class ComplexTableTest {
 
+    @Ignore
     @Test
     @DisplayName("Test de la validité du format du csv généré par l'extracteur Html")
-    public void htmlExtractorTest() throws MalformedURLException, InterruptedException {
-        //Url urlTest = new Url(new URL("https://en.wikipedia.org/wiki/Comparison_between_Ido_and_Novial"));
+    public void htmlExtractorTestSimple() throws MalformedURLException, InterruptedException {
+        Url urlTest = new Url(new URL("https://en.wikipedia.org/wiki/Comparison_between_Ido_and_Novial"));
+        assertTrue(urlTest.estTitreValide());
+        Donnee_Html donneeHtml = new Donnee_Html();
+        donneeHtml.setUrl(urlTest);
+        donneeHtml.start();
+        donneeHtml.join();
+
+        CSVValidator csvValidator = CSVValidator.getInstance();
+
+        assertTrue(csvValidator.checkCSVWithSeparator("HTML/" + urlTest.getTitre()+"-01.csv", ';'));
+        assertTrue(csvValidator.checkCSVWithSeparator("HTML/" + urlTest.getTitre()+"-11.csv", ';'));
+        assertTrue(csvValidator.checkCSVWithSeparator("HTML/" + urlTest.getTitre()+"-21.csv", ';'));
+    }
+
+
+    @Test
+    @DisplayName("Test de la validité du format du csv généré par l'extracteur Html")
+    public void htmlExtractorTestHard() throws MalformedURLException, InterruptedException {
         Url urlTest = new Url(new URL("https://en.wikipedia.org/wiki/List_of_AMD_graphics_processing_units"));
         assertTrue(urlTest.estTitreValide());
         Donnee_Html donneeHtml = new Donnee_Html();
